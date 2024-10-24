b6bdbc2654f31d53cee8cbd26e3877c87542a65b
Mohammad4229 <122114087+Mohammad4229@users.noreply.github.com>
2024-10-23 21:00:44 -0600

    Commented out the Intent

--- Appify/app/src/main/java/com/example/appify/MainActivity.java
+++ Appify/app/src/main/java/com/example/appify/MainActivity.java
@@ -94,8 +94,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animator animation) {
                 // CHANGE THE 'TempActivity' TO WHATEVER ACTIVITY YOU'RE WORKING ON
-                Intent intent = new Intent(MainActivity.this,TempActivity.class);
-                startActivity(intent);
+                // Intent intent = new Intent(MainActivity.this,TempActivity.class);
+                // startActivity(intent);
 
             }
         });
