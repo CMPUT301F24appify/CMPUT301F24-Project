9ce0b7465159093378ee43c4a7a4e18e8f4f88fe
Mohammad4229 <mjahangirh4229@gmail.com>
2024-10-31 12:02:29 -0600

    Added TempActivity

---
+++ Appify/app/src/main/java/com/example/appify/TempActivity.java
@@ -0,0 +1,11 @@
+package com.example.appify;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempActivity extends AppCompatActivity {
+    @Override
+    protected void  onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entrant_list_view);
+    }
+}
