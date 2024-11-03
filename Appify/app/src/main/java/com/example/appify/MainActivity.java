a09e1898a4900222f6bb409c8ca5e317bb0440b5
Mohammad4229 <mjahangirh4229@gmail.com>
2024-11-03 15:10:35 -0700

    uncommented intent

--- Appify/app/src/main/java/com/example/appify/MainActivity.java
+++ Appify/app/src/main/java/com/example/appify/MainActivity.java
@@ -93,9 +93,9 @@ public class MainActivity extends AppCompatActivity {
         firstSet.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                // CHANGE THE 'TempActivity' TO WHATEVER ACTIVITY YOU'RE WORKING ON
-                // Intent intent = new Intent(MainActivity.this,TempActivity.class);
-                // startActivity(intent);
+//                 CHANGE THE 'TempActivity' TO WHATEVER ACTIVITY YOU'RE WORKING ON
+                 Intent intent = new Intent(MainActivity.this,TempActivity.class);
+                 startActivity(intent);
 
             }
         });
