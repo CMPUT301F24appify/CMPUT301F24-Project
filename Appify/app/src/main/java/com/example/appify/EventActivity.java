8840855e787c83610c2c06403572c1c17f1519c6
NL727 <nhlin727@gmail.com>
2024-11-06 13:08:06 -0700

    update comments

--- Appify/app/src/main/java/com/example/appify/EventActivity.java
+++ Appify/app/src/main/java/com/example/appify/EventActivity.java
@@ -23,7 +23,7 @@ public class EventActivity extends AppCompatActivity implements AddEventDialogFr
     private static final int REQUEST_READ_EXTERNAL_STORAGE = 1;
     ListView eventListView;
     CustomEventAdapter eventAdapter;
-    ArrayList<Event> eventList;
+    ArrayList<Event> eventList = new ArrayList<>();
 
     // Sample events
     Event event1;
@@ -40,17 +40,17 @@ public class EventActivity extends AppCompatActivity implements AddEventDialogFr
         eventListView = findViewById(R.id.event_list);
 
         // Sample events
-        event1 = new Event(this,"Event 1", "Oct 12", "1", "2 days", "Here is the event description", 20, 5, null, false, false, false , false, false);
-        event2 = new Event(this,"Event 2", "Nov 23", "2", "3 days", "Some description", 200, 50, null, true, false, false, false, false);
-        event3 = new Event(this,"Event 3", "Jan 01", "3", "4 days", "blha blah blha", 3, 1, null, true, false, false, false, false);
-
-
-        Event []events = {event1,event2, event3};
-
-        System.out.println("test1");
-        eventList = new ArrayList<>();
-        eventList.addAll(Arrays.asList(events));
-        System.out.println("test2");
+//        event1 = new Event(this,"Event 1", "Oct 12", "1", "2 days", "Here is the event description", 20, 5, null, false, false, false , false, false);
+//        event2 = new Event(this,"Event 2", "Nov 23", "2", "3 days", "Some description", 200, 50, null, true, false, false, false, false);
+//        event3 = new Event(this,"Event 3", "Jan 01", "3", "4 days", "blha blah blha", 3, 1, null, true, false, false, false, false);
+//
+//
+//        Event []events = {event1,event2, event3};
+//
+//        System.out.println("test1");
+//        eventList = new ArrayList<>();
+//        eventList.addAll(Arrays.asList(events));
+//        System.out.println("test2");
         eventAdapter = new CustomEventAdapter(this,eventList);
         eventListView.setAdapter(eventAdapter);
 
@@ -99,24 +99,24 @@ public class EventActivity extends AppCompatActivity implements AddEventDialogFr
 
 
 
-        // Use the new method in Event
-        newEvent.addToFirestore(event -> {
-            Toast.makeText(EventActivity.this, "Event added: " + event.getName(), Toast.LENGTH_SHORT).show();
-            eventList.add(event);
-            eventAdapter.notifyDataSetChanged();
-        });
-//        String eventID = newEvent.getEventId();
-//        db.collection("events")
-//                .document(eventID)
-//                .set(newEvent)
-//                .addOnSuccessListener(aVoid -> {
-//                    Toast.makeText(EventActivity.this, "Event added: " + name, Toast.LENGTH_SHORT).show();
-//                    eventList.add(newEvent);
-//                    eventAdapter.notifyDataSetChanged();
-//                })
-//                .addOnFailureListener(e -> {
-//                    Toast.makeText(EventActivity.this, "Error adding event: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-//                });
+//        // Use the new method in Event
+//        newEvent.addToFirestore(event -> {
+//            Toast.makeText(EventActivity.this, "Event added: " + event.getName(), Toast.LENGTH_SHORT).show();
+//            eventList.add(event);
+//            eventAdapter.notifyDataSetChanged();
+//        });
+        String eventID = newEvent.getEventId();
+        db.collection("events")
+                .document(eventID)
+                .set(newEvent)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(EventActivity.this, "Event added: " + name, Toast.LENGTH_SHORT).show();
+                    eventList.add(newEvent);
+                    eventAdapter.notifyDataSetChanged();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(EventActivity.this, "Error adding event: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 
 
