aa29db592e7d8fdfd3427901291f1a34764562d6
Mohammad4229 <122114087+Mohammad4229@users.noreply.github.com>
2024-11-08 15:19:17 -0700

    Add  JavaDocs

---
+++ Docs/JavaDocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.appify"},{"l":"com.example.appify.Activities"},{"l":"com.example.appify.Adapters"},{"l":"com.example.appify.Fragments"},{"l":"com.example.appify.Model"}];updateSearchResults();
